25a4e3dee65623da747dc2df7a8d4e848125885a
QuintanilhaDev <mateusthequintanilha@gmail.com>
2023-01-24 09:14:48 -0300

    Put upgrades in Yellow Cat

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import git
+import os
 
 repo = git.Repo('pythonSecJourney-https')
 repo.remotes.origin.set_url('https://github.com/QuintanilhaDev/pythonSecJourney.git')
-
-archieve = open("pythonSecJourney-https/bla.txt", "r+")
-archieve.truncate(0)
-archieve.write('Yellow cat is here!')
-
-if repo.is_dirty(untracked_files=True):
-    print('*' * 50)
-    print('Meow! This repository have changes.🙀')
-    print(repo.untracked_files)
-    print('-' * 50)
-    print(f'Remote name: {repo.remotes.origin.name}')
-    print(f'Remote URL: {repo.remotes.origin.url}')
-    print('\nI am pushing the changes for you, wait a moment... 😼')
-
-    repo.index.add(repo.untracked_files)
-    repo.index.commit('Yellow cat is working here...🐱')
-    repo.git.push('--set-upstream', 'origin', 'main')
+archive = open("pythonSecJourney-https/bla.txt", "r+")
+archive.write('tag:1.0.8')
 
 
-    print('Meow! All is done here, i will back to sleep😽')
+def push_changes(url, tag):
 
-else:
-    print('Calm down, human... Have no changes here.😾')
+    if repo.is_dirty(untracked_files=True):
+        print('*' * 50)
+        print('Meow! This repository have changes.🙀')
+        print(repo.untracked_files)
+        print('-' * 50)
+        print(f'Remote name: {repo.remotes.origin.name}')
+        print(f'Remote URL: {repo.remotes.origin.url}')
+        print('\nI am pushing the changes for you, wait a moment... 😼')
+        repo.index.add(['bla.txt'])
+        repo.index.commit('Yellow cat is working here...🐱')
+        repo.git.push('-u', 'origin', 'main')
+
+
+        print('Meow! All is done here, i will back to sleep😽')
+
+    else:
+        print('Calm down, human... Have no changes here.😾')
+
+
+push_changes()
